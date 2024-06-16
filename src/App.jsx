dc20fd6a07622da4af9db5091e5379e129b475e9
abhuraya <149016492+abhuraya@users.noreply.github.com>
2024-06-16 16:12:21 -0400

    added components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Blog from './components/Blog.jsx'
+
+// initializes app and renders the body component
+function App() {
+  return (
+    <div>
+      <Blog />
+    </div>
+    
+  )
+
+}
+
+export default App
